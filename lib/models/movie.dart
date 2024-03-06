3cd94f610b6573c64c32bfb9c1e4abdbff4f90ef
TamimShidqi <mtamimshq@mhs.mdp.ac.id>
2024-03-06 08:55:56 +0700

    update movie

--- lib/models/movie.dart
+++ lib/models/movie.dart
@@ -18,13 +18,13 @@ class Movie {
 
   factory Movie.fromJson(Map<String, dynamic> json) {
     return Movie(
-      id: json['id'],
-      title: json['title'],
-      overview: json['overview'],
-      posterPath: json['poster_path'],
-      backdropPath: json['backdrop_path'],
-      releaseDate: json['release_date'],
-      voteAverage: json['vote_average'] as double,
+      id: json['id'] ?? 0,
+      title: json['title']??'',
+      overview: json['overview']??'',
+      posterPath: json['poster_path']??'',
+      backdropPath: json['backdrop_path']??'',
+      releaseDate: json['release_date']??'',
+      voteAverage: json['vote_average']??'0.0' as double,
     );
   }
 }
